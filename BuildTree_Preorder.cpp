eebb5c814a052a5f27af7040dbc3e9ea9f71b880
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2022-06-28 01:41:25 +0530

    Preorder Traversal

---
+++ BuildTree_Preorder.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int d)
+    {
+        data=d;
+        left=right=NULL;
+    }
+};
+
+//Preorder Traversal
+
+
+Node* buildtree()
+{
+    int d;
+    cin>>d;
+    if(d==-1)
+        return NULL;
+
+    Node* n= new Node(d);
+    n->left=buildtree();
+    n->right=buildtree();
+    return n;
+
+}
+
+void printpreorder(Node *root)
+{
+    if(root==NULL)
+        return;
+    cout<<root->data<<" ";
+    printpreorder(root->left);
+    printpreorder(root->right);
+}
+
+int main()
+{
+    Node* root=buildtree();
+    printpreorder(root);
+
+}
