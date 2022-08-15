0d42d9b7b840de4cc07892a17c57290f70fda329
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2022-08-15 22:45:27 +0530

    Trees

---
+++ Trees/ChildrenSumProperty.cpp
@@ -0,0 +1,34 @@
+class Solution{
+public:
+
+    void changeTree(TreeNode* root){
+        if(!root)
+            return ;
+        int child=0;
+        if(root->left){
+            child+=root->left->val;
+        }
+        if(root->right){
+            child+=root->right->val;
+        }
+
+        if(child>=root->val)
+            root->val=child;
+        else{
+            if(root->left)
+                root->left->val=root->val;
+            else if(root->right)
+                root->right->val=root->val;
+        }
+        changeTree(root->left);
+        changeTree(root->right);
+
+        int tot=0;
+        if(root->left)
+            tot+=root->left->val;
+        if(root->right)
+            tot+=root->right->val;
+        if(root->left or root->right)
+            root->val=tot;
+    }
+};
